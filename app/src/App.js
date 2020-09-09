cab0c52980a57cfde2f73a7579f1c1d6351b3f3a
DESKTOP-F6M9PI8\lamun <galunyu@gmail.com>
2020-09-10 00:45:42 +0700

    [Edited] cached app folder

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="header_left"></div>
+      <div className="header_middle"></div>
+      <div className="header_right"></div>
+    </div>
+  );
+};
+
+export default App;
